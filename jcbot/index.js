1520582e48827714b94b90d6c5d051698b5e91c9
John Collier <52043202+jpcollier@users.noreply.github.com>
2023-04-13 09:21:40 -0400

    Add files via upload

--- jcbot/index.js
+++ jcbot/index.js
@@ -1,7 +1,7 @@
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+for (var i = 0; i < document.querySelectorAll(".square").length; i++) {
 
-    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+    document.querySelectorAll(".square")[i].addEventListener("click",function() {
 
         var buttonInnerHTML = this.innerHTML;
 
